013876597d743259e7ca4260cc0dce96cc6ade2b
Hekateras <39839566+Hekateras@users.noreply.github.com>
2020-06-03 18:55:02 +0200

    Tech Expo update

--- scripts/propdefs.lua
+++ scripts/propdefs.lua
@@ -1,6 +1,7 @@
 local util = include( "modules/util" )
 local simdefs = include( "sim/simdefs" )
 local commondefs = include("sim/unitdefs/commondefs")
+local itemdefs = include("sim/unitdefs/itemdefs")
 local tool_templates = include("sim/unitdefs/itemdefs")
 
 -------------------------------------------------------------
@@ -67,26 +68,175 @@ local prop_templates =
 			-- computer_reset="SpySociety_DLC001/Actions/DLCswitch_computerreset",
 		-- }
 	-- },
+	
+	--custom one so no overlap
+	MM_hostage_capture_ea = 
+	{ 
+		type = "simunit", 
+		name = STRINGS.PROPS.HOSTAGE,
+		rig = "hostagerig",	
+		onWorldTooltip = function( tooltip, unit )
+			tooltip:addLine( unit:getName() )
+			tooltip:addAbility( STRINGS.ABILITIES.RESCUE, STRINGS.ABILITIES.RESCUE_HOSTAGE_DESC, "gui/items/icon-action_open-safe.png",nil,true )
+		end,
+		tags = { "MM_hostage" },
+		kanim = "kanim_hostage", --this will be custom
 
+		traits = { impass = {0,0}, rescue_incident = "hostage_rescued", template="hostage",  mp=5, mpMax =5, sightable = true, MM_hostage = true, untie_anim = true,  vitalSigns = 2, agent_filter= true }, 
+		abilities = { "hostage_rescuable" },
+		sounds = {appeared="SpySociety/HUD/gameplay/peek_positive", }
+	},	
+	
+	-- WEAPONS EXPO
+
+	-- in mission script, pick kanim at random from the 3 available on spawn
+	MM_prototype_droid_prop = 
+	{ 
+		type = "simunit", 
+		name = STRINGS.MOREMISSIONS.GUARDS.PROTOTYPE_DROID, --"Android Prototype"
+		rig = "unitrig",
+		tooltip = nil,
+		tags = {"MM_droid_dummy"},
+		kanim = "mm_kanim_guard_male_dummy1", 
+		traits = { impass = {0,0}, sightable = true, staticAnim  = true, MM_droid_dummy = true},
+		sounds = {appeared="SpySociety/HUD/gameplay/peek_positive", },
+	},
+	
+	MM_prototype_specdroid_prop = 
+	{ 
+		type = "simunit", 
+		name = STRINGS.MOREMISSIONS.GUARDS.PROTOTYPE_DROID, --"Android Prototype"
+		rig = "unitrig",
+		tooltip = nil,
+		tags = {"MM_droid_dummy"},
+		kanim = "mm_kanim_guard_male_dummy2", 
+		traits = { impass = {0,0}, sightable = true, staticAnim  = true, MM_droid_dummy = true, spec_droid = true},
+		sounds = {appeared="SpySociety/HUD/gameplay/peek_positive", },
+	},	
+	
+	MM_portal_turret =
+	{
+		type = "simunit",
+		name = STRINGS.PROPS.TURRET,
+		tooltip = nil,
+		nolocator = true,
+		profile_anim = "portraits/turret_portrait",
+		kanim = "kanim_MM_portal_turret", --give custom, pick random in mission
+		rig = "turretrig",
+		-- facing = simdefs.DIR_E,		
+		traits = {sightable = true, impass = {0,0}, cover = true,},
+		abilities = {},
+		children = { },
+	},
+	
+	--this looks like a custom turret but can't be activated
+	MM_turret_prop =
+	{
+		type = "simunit",
+		name = STRINGS.PROPS.TURRET,
+		tooltip = nil,
+		nolocator = true,
+		profile_anim = "portraits/turret_portrait",
+		kanim = "kanim_MM_prototype_turret", --give custom, pick random in mission
+		rig = "turretrig",
+		-- facing = simdefs.DIR_E,		
+		traits = {sightable = true, impass = {0,0}, cover = true,},
+		abilities = {},
+		children = { },
+	},
+
+	MM_drone_prop = 
+	{ 
+		type = "simunit", 
+		name = STRINGS.PROPS.CAPTURED_AGENT, --"Drone"
+		rig = "unitrig",
+		tooltip = nil,
+		kanim = "kanim_MM_prototype_drone", --give custom!
+		traits = { impass = {0,0}, sightable = true, cover = true,},
+	},
+	
+	--need custom one that looks distinct from side mission switch
+	MM_lock_switch = 
+	{
+		type = "simunit", 
+		name = STRINGS.DLC1.PROPS.LOCK_SWITCH,
+		rig ="corerig",
+		onWorldTooltip = onDeviceTooltip,
+		kanim = "kanim_switch", 
+		abilities = { "multiUnlock"  }, 
+		traits = util.extend( MAINFRAME_TRAITS ){ 
+			moveToDevice=true, 
+			cover = true, 
+			impass = {0,0}, 
+			sightable=true,
+			multiLockSwitch = true, 
+			recap_icon = "access_switch",
+			mainframe_no_daemon_spawn = false,
+			mainframe_always_daemon_spawn = true, --we don't want it to be too easy :^)	
+			},
+			--multilockswitch is used in unitrig for FX
+			--MM_multilock trait is used in script and set in prefab file, NOT here in propdef
+		sounds = {appeared="SpySociety/HUD/gameplay/peek_positive", reboot_start="SpySociety/Actions/reboot_initiated_generator",reboot_end="SpySociety/Actions/reboot_complete_generator", switch_reset="SpySociety_DLC001/Actions/DLCswitch_reset", computer_reset="SpySociety_DLC001/Actions/DLCswitch_computerreset" }
+	},	
+
+	
+	MM_vault_safe_1 = 
+	{ 
+		type = "simunit", 
+		name = STRINGS.PROPS.SECURE_CASE,
+		onWorldTooltip = onSafeTooltip,
+		kanim = "kanim_MM_gunsafe_1", 
+		rig ="corerig",
+		tags = {"MM_topGear"},
+		traits = util.extend( SAFE_TRAITS, MAINFRAME_TRAITS ) {moveToDevice=true, MM_emp_safe = true, mainframe_ice = 2, mainframe_iceMax = 2,},
+		abilities = { "stealCredits" },
+		sounds = {appeared="SpySociety/HUD/gameplay/peek_positive",reboot_start="SpySociety/Actions/reboot_initiated_safe",reboot_end="SpySociety/Actions/reboot_complete_safe" },
+		-- children = {"item_valuable_tech"},
+	},	
+
+	MM_vault_safe_2 = 
+	{ 
+		type = "simunit", 
+		name = STRINGS.PROPS.SECURE_CASE,
+		onWorldTooltip = onSafeTooltip,
+		kanim = "kanim_MM_gunsafe_2", 
+		rig ="corerig",
+		tags = {"MM_topGear"},
+		traits = util.extend( SAFE_TRAITS, MAINFRAME_TRAITS ) {moveToDevice=true, MM_emp_safe = true, mainframe_ice = 2, mainframe_iceMax = 2,},
+		abilities = { "stealCredits" },
+		sounds = {appeared="SpySociety/HUD/gameplay/peek_positive",reboot_start="SpySociety/Actions/reboot_initiated_safe",reboot_end="SpySociety/Actions/reboot_complete_safe" },
+		-- children = {"item_valuable_tech"},
+	},		
+	
+	MM_vault_safe_3 = 
+	{ 
+		type = "simunit", 
+		name = STRINGS.PROPS.SECURE_CASE,
+		onWorldTooltip = onSafeTooltip,
+		kanim = "kanim_MM_gunsafe_3", 
+		rig ="corerig",
+		tags = {"MM_topGear"},
+		traits = util.extend( SAFE_TRAITS, MAINFRAME_TRAITS ) {moveToDevice=true, MM_emp_safe = true, mainframe_ice = 2, mainframe_iceMax = 2,},
+		abilities = { "stealCredits" },
+		sounds = {appeared="SpySociety/HUD/gameplay/peek_positive",reboot_start="SpySociety/Actions/reboot_initiated_safe",reboot_end="SpySociety/Actions/reboot_complete_safe" },
+		-- children = {"item_valuable_tech"},
+	},		
+	
+	MM_cell_door = 
+	{
+		type = "simunit", 
+		name =  "Exhibit Case",--STRINGS.PROPS.CELL_DOOR,
+		rig ="corerig",
+		onWorldTooltip = function( tooltip, unit )
+			tooltip:addLine( unit:getName() )
+		end,
+		kanim = "kanim_celldoor1", 
+		traits = {  impass = {0,0}, sightable=true, cell_door=true },
+		sounds = { }
+	},		
+		
 }
 
---custom one so no overlap
-MM_hostage_capture_ea = 
-{ 
-	type = "simunit", 
-	name = STRINGS.PROPS.HOSTAGE,
-	rig = "hostagerig",	
-	onWorldTooltip = function( tooltip, unit )
-		tooltip:addLine( unit:getName() )
-		tooltip:addAbility( STRINGS.ABILITIES.RESCUE, STRINGS.ABILITIES.RESCUE_HOSTAGE_DESC, "gui/items/icon-action_open-safe.png",nil,true )
-	end,
-	tags = { "MM_hostage" },
-	kanim = "kanim_hostage", --this will be custom
-
-	traits = { impass = {0,0}, rescue_incident = "hostage_rescued", template="MM_hostage",  mp=5, mpMax =5, sightable = true, MM_hostage = true, untie_anim = true,  vitalSigns = 2, agent_filter= true }, 
-	abilities = { "hostage_rescuable" },
-	sounds = {appeared="SpySociety/HUD/gameplay/peek_positive", }
-},	
 
 -- Reassign key name to value table.
 for id, template in pairs(prop_templates) do
