bb053af4fbc2b9fe29071875f83596541bd1b4d1
Hekateras <39839566+Hekateras@users.noreply.github.com>
2020-09-19 23:32:34 +0200

    side mission nanofab update

--- scripts/modinit.lua
+++ scripts/modinit.lua
@@ -96,7 +96,7 @@ local function init( modApi )
 	
 	showItemStore.executeAbility = function( self, sim, unit, userUnit, ... )
 	-- note: unit is nanofab, userUnit is agent
-		if unit:getTraits().storeType and (unit:getTraits().storeType == "large") then
+		if unit:getTraits().storeType and (unit:getTraits().storeType == "large") and unit:getTraits().luxuryNanofab and sim.luxuryNanofabItemType then
 			
 			local strings_screens = include( "strings_screens" )
 			sim.old_augmenttip, sim.old_weapontip, sim.old_itemtip = strings_screens.STR_346165218, strings_screens.STR_2618909495, strings_screens.STR_590530336
