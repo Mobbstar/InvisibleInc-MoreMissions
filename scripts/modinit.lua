2be6a1adffec6eb7857dd0b2341d52bd209af95f
Qoalabear <512290+qoala@users.noreply.github.com>
2020-09-21 00:58:21 -1000

    Disable holostudio by default
    Its unique items aren't implemented yet. Disable it to let playtesters
    focus on the more fully implemented missions.

--- scripts/modinit.lua
+++ scripts/modinit.lua
@@ -39,7 +39,7 @@ local function init( modApi )
 	modApi:addGenerationOption("vault",  STRINGS.MOREMISSIONS.OPTIONS.VAULT , STRINGS.MOREMISSIONS.OPTIONS.VAULT_TIP, {noUpdate=true} )
 	--I really wish there were some kind of splitter right about now -M
 	--Shirsh set those that not works to "false" until they'll be ready to not bug test playthroughs
-	modApi:addGenerationOption("holostudio",  STRINGS.MOREMISSIONS.OPTIONS.HOLOSTUDIO , STRINGS.MOREMISSIONS.OPTIONS.HOLOSTUDIO_TIP, {noUpdate=true} )
+	modApi:addGenerationOption("holostudio",  STRINGS.MOREMISSIONS.OPTIONS.HOLOSTUDIO , STRINGS.MOREMISSIONS.OPTIONS.HOLOSTUDIO_TIP, {noUpdate=true, enabled = false} )
 	modApi:addGenerationOption("assassination",  STRINGS.MOREMISSIONS.OPTIONS.ASSASSINATION , STRINGS.MOREMISSIONS.OPTIONS.ASSASSINATION_TIP, {noUpdate=true} )
 	modApi:addGenerationOption("landfill",  STRINGS.MOREMISSIONS.OPTIONS.LANDFILL , STRINGS.MOREMISSIONS.OPTIONS.LANDFILL_TIP, {noUpdate=true, enabled = false} )
 
