b7066aa085c57a466d00f3267e2b682a3348ffde
Qoalabear <512290+qoala@users.noreply.github.com>
2020-10-07 21:41:52 -1000

    Add *nix-based automation for anims, kwads, and scripts.zip
    Updates the build.lua to build moremissions_anims.lua and skip the empty
    sounds.lua

---
+++ makeconfig.example.mk
@@ -0,0 +1,10 @@
+# Local config for Makefile
+#
+# Copy this as makeconfig.mk and edit that file for the paths on your system.
+
+II_INSTALL_ROOT=~/code/invisinc/install
+
+KWAD_BUILDER=~/kwad-builder/bin/linux/builder
+INSTALL_PATH=$(REPLACEME)/steamapps/common/InvisibleInc/mods/moremissions
+# Uncomment the following line if you maintain separate "all mods" and "active mods" folders
+# INSTALL_PATH2=
