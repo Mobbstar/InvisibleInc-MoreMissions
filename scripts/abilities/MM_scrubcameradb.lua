dd0a1add09b5c659f448f8e46ebe9f60513c25e7
Hekateras <39839566+Hekateras@users.noreply.github.com>
2021-04-08 21:49:44 +0200

    mole update

---
+++ scripts/abilities/MM_scrubcameradb.lua
@@ -0,0 +1,150 @@
+local array = include( "modules/array" )
+local util = include( "modules/util" )
+local cdefs = include( "client_defs" )
+local simdefs = include("sim/simdefs")
+local simquery = include("sim/simquery")
+local abilityutil = include( "sim/abilities/abilityutil" )
+
+local function existWitnesses( sim )
+	for i, unit in pairs(sim:getAllUnits()) do
+		if unit:getTraits().witness and (unit:getTraits().mainframe_camera) then
+			return true
+		end
+	end
+	return false
+end
+
+local function clearWitnesses( sim )
+	for i, unit in pairs(sim:getAllUnits()) do
+		if unit:getTraits().witness and unit:getTraits().mainframe_camera then
+			unit:getTraits().witness = nil
+		end
+	end
+end
+
+local MM_scrubcameradb = --tweaked version of monster root hub hack
+	{
+		name = STRINGS.MOREMISSIONS.ABILITIES.SCRUB_CAMERADB,
+
+		createToolTip = function( self, sim, unit )
+			return abilityutil.formatToolTip(STRINGS.MOREMISSIONS.ABILITIES.SCRUB_CAMERADB,STRINGS.MOREMISSIONS.ABILITIES.SCRUB_CAMERADB_DESC) 
+		end,
+
+		proxy = true,
+
+		getName = function( self, sim, abilityOwner, abilityUser, targetUnitID )
+			return self.name
+		end,
+		
+		alwaysShow = true,
+		
+		profile_icon = "gui/icons/action_icons/Action_icon_Small/icon-item_hijack_small.png",
+
+		isTarget = function( self, abilityOwner, unit, targetUnit )
+		
+			if targetUnit:getTraits().mainframe_status ~= "active" then
+				return false
+			end
+			
+			return true
+		end,
+
+		acquireTargets = function( self, targets, game, sim, abilityOwner, unit )
+			local units = {}
+			
+			-- if sim:getTags().finished_DB_hack then
+				-- return targets.unitTarget( game, units, self, abilityOwner, unit )
+			-- end 
+
+			for _, targetUnit in pairs(sim:getAllUnits()) do
+				local x1, y1 = targetUnit:getLocation()
+				if x1 and self:isTarget( abilityOwner, unit, targetUnit ) and simquery.canUnitReach( sim, unit, x1, y1 ) then
+					table.insert( units, targetUnit )
+				end
+			end
+
+			return targets.unitTarget( game, units, self, abilityOwner, unit )
+		end,
+
+		canUseAbility = function( self, sim, abilityOwner, unit, targetUnitID )
+			local targetUnit = sim:getUnit( targetUnitID )
+			local userUnit = abilityOwner:getUnitOwner()
+
+			if abilityOwner:getTraits().cooldown and abilityOwner:getTraits().cooldown > 0 then
+				return false,  util.sformat(STRINGS.UI.REASON.COOLDOWN,abilityOwner:getTraits().cooldown)
+			end	
+			if abilityOwner:getTraits().usesCharges and abilityOwner:getTraits().charges < 1 then
+				return false, util.sformat(STRINGS.UI.REASON.CHARGES)
+			end				
+
+			if unit:getTraits().monster_hacking == true then 
+				return false, STRINGS.UI.REASON.ALREADY_HACKING 
+			end 
+
+			if unit:getTraits().MM_mole ~= true then 
+				return false, STRINGS.ABILITIES.HACK_ONLY_MOLE --only mole
+			end 
+
+			if abilityOwner:getTraits().mainframe_ice > 0 then 
+				return false, STRINGS.ABILITIES.TOOLTIPS.UNLOCK_WITH_INCOGNITA
+			end
+			
+			if not existWitnesses(sim) then
+				return false, STRINGS.MOREMISSIONS.UI.NO_CAMERADB_WITNESSES
+			end
+
+			return abilityutil.checkRequirements( abilityOwner, userUnit )
+		end,
+		
+		onSpawnAbility = function( self, sim, unit )
+			self.abilityOwner = unit
+			sim:addTrigger( simdefs.TRG_START_TURN, self )
+		end,
+		
+		onDespawnAbility = function( self, sim, unit )
+			self.abilityOwner = nil
+			sim:removeTrigger( simdefs.TRG_START_TURN, self )
+		end,		
+		
+		onTrigger = function( self, sim, evType, evData )
+			-- warp stuff taken care of in simunit.stopHacking
+			if (evType == simdefs.TRG_START_TURN) and evData:isPC() then
+				local hacker = sim:getUnit( self.abilityOwner:getTraits().MM_hacker )
+
+				if hacker and ( hacker:getTraits().monster_hacking == self.abilityOwner:getID() ) then
+					-- log:write("LOG clearing witnesses")
+					clearWitnesses( sim )
+					local x0, y0 = self.abilityOwner:getLocation()
+					sim:dispatchEvent( simdefs.EV_UNIT_FLOAT_TXT, {txt=util.sformat(STRINGS.MOREMISSIONS.UI.CAMERADB_SCRUBBED),x=x0,y=y0,color={r=1,g=1,b=1,a=1}} )
+					hacker:getTraits().monster_hacking = nil
+					self.abilityOwner:getTraits().MM_hacker = nil
+					hacker:getSounds().spot = nil
+					sim:dispatchEvent( simdefs.EV_UNIT_TINKER_END, { unit = hacker } )					
+					sim:dispatchEvent( simdefs.EV_UNIT_REFRESH, { unit= hacker } )
+					sim:triggerEvent( "cameradb_scrubbed" )
+				end
+			end
+		
+		end,
+		
+		executeAbility = function( self, sim, abilityOwner, unit, targetUnitID )
+			local targetUnit = sim:getUnit( targetUnitID )
+
+			local x0,y0 = unit:getLocation()
+			local x1,y1 = targetUnit:getLocation()			
+			local tempfacing = simquery.getDirectionFromDelta( x1 - x0, y1 - y0 )
+			unit:setFacing(tempfacing)	
+			sim:dispatchEvent( simdefs.EV_UNIT_USECOMP, { unitID = unit:getID(), facing = tempfacing, sound = "SpySociety/Actions/monst3r_jackin" , soundFrame = 16, useTinkerMonst3r=true } )
+			-- sim:triggerEvent( "mole_DB_hack_start" )
+
+			unit:getTraits().monster_hacking = abilityOwner:getID() --trait to set looped tinker anim
+			abilityOwner:getTraits().MM_hacker = unit:getID()
+			
+			-- self.abilityOwner:getTraits().progress = abilityOwner:getTraits().progress or 0 			
+			unit:getSounds().spot = "SpySociety/Actions/monst3r_hacking"
+			sim:dispatchEvent( simdefs.EV_UNIT_REFRESH, { unit = unit })
+		end,
+
+	}
+	
+return MM_scrubcameradb
---
+++ scripts/abilities/npc_abilities.lua
@@ -0,0 +1,176 @@
+local mathutil = include( "modules/mathutil" )
+local array = include( "modules/array" )
+local util = include( "modules/util" )
+local simdefs = include("sim/simdefs")
+local simquery = include("sim/simquery")
+local cdefs = include( "client_defs" )
+local mainframe = include( "sim/mainframe" )
+local modifiers = include( "sim/modifiers" )
+local mission_util = include( "sim/missions/mission_util" )
+local serverdefs = include("modules/serverdefs")
+local mainframe_common = include("sim/abilities/mainframe_common")
+local unitghost = include( "sim/unitghost" )
+local simfactory = include( "sim/simfactory" )
+local unitdefs = include( "sim/unitdefs" )
+
+local existsWitnessOnMap = function(sim)
+	--Guard or drone that has directly seen an agent.
+	for unitID, unit in pairs(sim:getAllUnits()) do
+		if unit:getTraits().witness and not unit:isDead() then
+			return true
+		end
+	end
+	return false
+end
+
+local checkAllWitnesses = function( self )
+	return self.camera_witnesses + self.guard_witnesses + self.drone_witnesses + self.escaped_witnesses
+end
+
+local TRG_DATABASE_SCRUBBED = "MM_DB_scrubbed"
+
+local npc_abilities = 
+{
+
+	MM_informant_intel = util.extend( mainframe_common.createReverseDaemon( STRINGS.MOREMISSIONS.DAEMONS.MOLE_DAEMON ) ) --this daemon is basically cosmetic/for UI convenience only
+	{
+		icon = "gui/icons/UI_icons/icon_program_moleBonus.png",--icon
+		title = STRINGS.MOREMISSIONS.DAEMONS.MOLE_DAEMON_NAME,
+		noDaemonReversal = true,
+		bonus_type = "",
+		missionsLeft = 0,
+
+		onTooltip = function( self, hud, sim, player )
+            local tooltip = util.tooltip( hud._screen )
+			local section = tooltip:addSection()
+			
+			section:addLine( self.name )
+			section:addAbility( STRINGS.MOREMISSIONS.DAEMONS.MOLE_DAEMON_EVENT.INTEL_TYPES[self.bonus_type], util.sformat(STRINGS.MOREMISSIONS.DAEMONS.MOLE_DAEMON.DESC, self.missionsLeft), "gui/icons/action_icons/Action_icon_Small/actionicon_talk.png" )		
+			if self.dlcFooter then
+				section:addFooter(self.dlcFooter[1],self.dlcFooter[2])
+			end
+
+            return tooltip
+		end,
+		
+		onSpawnAbility = function( self, sim, player, agent )
+			-- sim:dispatchEvent( simdefs.EV_SHOW_REVERSE_DAEMON, { showMainframe=true, name=self.name, icon=self.icon, txt=self.activedesc, title=self.title } )
+		end,
+		
+		onDespawnAbility = function( self, sim )
+		end,
+	},
+
+	MM_informant_witness = util.extend( mainframe_common.createDaemon( STRINGS.MOREMISSIONS.DAEMONS.WITNESS_WARNING ) ) --this daemon is basically cosmetic/for UI convenience only
+	{
+		icon = "gui/icons/UI_icons/icon_program_moleWitness.png",--icon
+		title = STRINGS.MOREMISSIONS.DAEMONS.MOLE_DAEMON_NAME,
+		
+		onTooltip = function( self, hud, sim, player )
+            local tooltip = util.tooltip( hud._screen )
+			local section = tooltip:addSection()
+			section:addLine( self.name )
+			section:addAbility( STRINGS.MOREMISSIONS.DAEMONS.WITNESS_WARNING.ACTIVE_DESC, STRINGS.MOREMISSIONS.DAEMONS.WITNESS_WARNING.DESC, "gui/items/icon-action_peek.png" )
+			-----
+			section:addAbility( STRINGS.MOREMISSIONS.DAEMONS.WITNESS_WARNING.GUARDS, util.sformat(STRINGS.MOREMISSIONS.DAEMONS.WITNESS_WARNING.WITNESSES_LEFT_GUARDS, self.guard_witnesses), "gui/icons/action_icons/Action_icon_Small/icon-item_shoot_small.png" )
+			
+			section:addAbility( STRINGS.MOREMISSIONS.DAEMONS.WITNESS_WARNING.CAMERAS, util.sformat(STRINGS.MOREMISSIONS.DAEMONS.WITNESS_WARNING.WITNESSES_LEFT_CAMERAS, self.camera_witnesses), "gui/icons/action_icons/Action_icon_Small/icon-item_shoot_small.png" )
+			
+			-- if sim:getParams().difficultyOptions.enable_devices then -- check for Worldgen Extended's Drone Uplinks, need to also check if it is actually present
+			-- too complex/inconsistent, leave this out for now
+				-- section:addAbility( STRINGS.MOREMISSIONS.DAEMONS.WITNESS_WARNING.DRONES, util.sformat(STRINGS.MOREMISSIONS.DAEMONS.WITNESS_WARNING.WITNESSES_LEFT_DRONES_WE, self.drone_witnesses), "gui/icons/action_icons/Action_icon_Small/icon-item_shoot_small.png" )			
+			-- else
+				section:addAbility( STRINGS.MOREMISSIONS.DAEMONS.WITNESS_WARNING.DRONES, util.sformat(STRINGS.MOREMISSIONS.DAEMONS.WITNESS_WARNING.WITNESSES_LEFT_DRONES, self.drone_witnesses), "gui/icons/action_icons/Action_icon_Small/icon-item_shoot_small.png" )
+			-- end
+			------
+			if self.escaped_witnesses > 0 then
+				section:addAbility( STRINGS.MOREMISSIONS.DAEMONS.WITNESS_WARNING.ESCAPED, util.sformat(STRINGS.MOREMISSIONS.DAEMONS.WITNESS_WARNING.WITNESSES_ESCAPED, self.escaped_witnesses), "gui/icons/thought_icons/status_run.png" )
+			
+			end
+			
+			if self.dlcFooter then
+				section:addFooter(self.dlcFooter[1],self.dlcFooter[2])
+			end
+
+            return tooltip
+        end,	
+		
+		noDaemonReversal = true,
+		
+		onSpawnAbility = function( self, sim, player, agent )
+			sim:addTrigger( simdefs.TRG_UNIT_KILLED, self )
+			sim:addTrigger( simdefs.TRG_UNIT_PARALYZED , self )
+			sim:addTrigger( TRG_DATABASE_SCRUBBED , self )
+			sim:addTrigger( simdefs.TRG_UNIT_APPEARED , self )
+			sim:addTrigger( simdefs.TRG_START_TURN , self ) --just in case
+			sim:addTrigger( "cameradb_scrubbed", self )
+			sim:addTrigger( "mole_final_escape", self )
+			sim:addTrigger( "vip_escaped", self )
+			self.camera_witnesses = 0
+			self.guard_witnesses = 0
+			self.drone_witnesses = 0
+			self.escaped_witnesses = 0
+			-- sim:dispatchEvent( simdefs.EV_SHOW_REVERSE_DAEMON, { showMainframe=true, name=self.name, icon=self.icon, txt=self.activedesc, title=self.title } )
+		end,
+		
+		onDespawnAbility = function( self, sim )
+			sim:removeTrigger( simdefs.TRG_UNIT_KILLED, self )
+			sim:removeTrigger( simdefs.TRG_UNIT_PARALYZED, self )
+			sim:removeTrigger( TRG_DATABASE_SCRUBBED, self )
+			sim:removeTrigger( simdefs.TRG_UNIT_APPEARED , self )
+			sim:removeTrigger( simdefs.TRG_START_TURN , self )
+			sim:removeTrigger( "cameradb_scrubbed", self )
+			sim:removeTrigger( "mole_final_escape", self )
+			sim:removeTrigger( "vip_escaped", self )
+		end,
+		
+		onTrigger = function( self, sim, evType, evData, userUnit )
+			-- if (evType == simdefs.TRG.UNIT_KILLED) or (evType == simdefs.TRG_UNIT_PARALYZED) or (evType == TRG_DATABASE_SCRUBBED) or (evType == simdefs.TRG_UNIT_APPEARED) then --refresh after any event that could lead to witness number changing, this should be fairly infrequent
+				local camera_witnesses = {}
+				local guard_witnesses = {}
+				local drone_witnesses = {}
+
+				for unitID, unit in pairs(sim:getAllUnits()) do
+					if unit:getTraits().witness then
+						if unit:getTraits().isGuard and not unit:getTraits().isDrone then
+							table.insert(guard_witnesses, unit)
+						end
+						if unit:getTraits().isDrone then
+							table.insert(drone_witnesses, unit)
+						end
+						if unit:getTraits().mainframe_camera then
+							table.insert(camera_witnesses, unit)
+						end
+					end
+				end	
+				self.camera_witnesses = #camera_witnesses
+				self.guard_witnesses = #guard_witnesses
+				self.drone_witnesses = #drone_witnesses
+			-- end
+			if evType == "mole_final_escape" then
+				if checkAllWitnesses(self) <= 0 then
+					sim:getNPC():removeAbility(sim, "MM_informant_witness")  --despawn self if no witnesses left and mole has escaped
+				end
+			elseif evType == simdefs.TRG_UNIT_ESCAPED then
+				local unit = evData
+				if unit and unit:getTraits().MM_mole then
+					if checkAllWitnesses(self) <= 0 then
+						sim:getNPC():removeAbility(sim, 		"MM_informant_witness")  --despawn self if no witnesses left and mole has escaped
+					end
+				end
+			elseif evType == "vip_escaped" then --for pacifists who can flee
+				local unit = evData.unit
+				if unit:getTraits().witness then --for agent elevator "escape"
+					self.escaped_witnesses = self.escaped_witnesses + 1
+					sim:getTags().MM_escapedWitness = true
+				end
+			end
+			mainframe_common.DEFAULT_ABILITY.onTrigger( self, sim, evType, evData, userUnit )			
+		end,
+	},
+	
+}
+
+-- sim:triggerEvent( simdefs.TRG_UNIT_PARALYZED )
+
+return npc_abilities
