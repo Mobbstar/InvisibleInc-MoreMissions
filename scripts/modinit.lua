a0d8c0a6f85705fa8271cc827a5b6a9310a2c66b
Hekateras <39839566+Hekateras@users.noreply.github.com>
2021-04-12 00:55:46 +0200

    generatePrefabs append for mole

--- scripts/modinit.lua
+++ scripts/modinit.lua
@@ -117,6 +117,29 @@ local function init( modApi )
 		
 		showItemStore_executeOld(self, sim, unit, userUnit, ...)
 	end	
+				
+	-- inverted spawn fitness check allows existing generatePrefabs worldgen functions to maximise distance to a prefab tag specified in mission.
+	local prefabs = include("sim/prefabs")
+	local generatePrefabs_old = prefabs.generatePrefabs
+	prefabs.generatePrefabs = function( cxt, candidates, tag, maxCount, fitnessFn, fitnessSelect, ... )
+		log:write("LOG prefabs")
+		if not fitnessFn and cxt.defaultFitnessFn and cxt.defaultFitnessSelect and cxt.defaultFitnessFn[tag] and cxt.defaultFitnessSelect[tag] then
+			fitnessFn = cxt.defaultFitnessFn[tag]
+			fitnessSelect = cxt.defaultFitnessSelect[tag]
+			if cxt.maxCountOverride[tag] then
+				maxCount = cxt.maxCountOverride[tag]
+			end
+			-- local oldMaxCount = maxCount
+			-- maxCount = 1
+			-- for i = (oldMaxCount - 1), 1, -1 do
+				-- generatePrefabs_old( cxt, candidates, tag, maxCount, fitnessFn, fitnessSelect, ... ) --needed so BOTH guard exits spawn far from mole prefab. Re-run function as many times as original maxCount
+			-- end
+		end
+
+		generatePrefabs_old( cxt, candidates, tag, maxCount, fitnessFn, fitnessSelect, ... )
+	end
+
+	-- END OF MOLE INSERTION	
 	
 	include( scriptPath .. "/simquery" )
 	include( scriptPath .. "/engine" )
@@ -261,8 +284,7 @@ local function lateInit( modApi )
 		end		
 		--Funky Library takes care of impair AP stuff
 	end
-	-- END OF MOLE INSERTION
-
+	
 end
 
 --The implementation of array.removeAllElements is not optimal for our purposes, and we also need something to remove dupes, so might as well combine it all. -M
