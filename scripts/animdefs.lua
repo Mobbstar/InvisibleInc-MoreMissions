8e668b3cc980a8c30714a7631d6205999d75fa6b
Hekateras <39839566+Hekateras@users.noreply.github.com>
2021-05-09 02:30:11 +0200

    agent 009 animdef

--- scripts/animdefs.lua
+++ scripts/animdefs.lua
@@ -376,6 +376,41 @@ local animdefs =
 		-- boundType = BoundType.Character, -- this doesn't really apply to HUD stuff...
 	-- },	
 	
+	kanim_MM_agent009 =
+	{
+		wireframe =
+		{
+			"data/anims/characters/agents/overlay_agent_shalem.abld",
+		},
+		build = 
+		{ 
+			"data/anims/characters/agents/MM_agent009.abld",
+			"data/anims/characters/anims_male/shared_hits_01.abld",	
+			"data/anims/characters/anims_male/shared_attacks_a_01.abld",	 	 
+		},
+		grp_build = 
+		{
+			"data/anims/characters/agents/grp_MM_agent009.abld",
+		},
+		grp_anims = commonanims.male.grp_anims,
+
+		anims = commonanims.male.default_anims_unarmed,
+		anims_1h = commonanims.male.default_anims_1h,
+		anims_2h = commonanims.male.default_anims_2h,
+		animMap = AGENT_ANIMS,
+
+		symbol = "character",
+		anim = "idle",
+		shouldFlip = true,
+		scale = 0.25,
+		layer = Layer.Unit,
+		boundType = BoundType.Character,
+		boundTypeOverrides = {			
+			{anim="idle_ko" ,boundType= BoundType.CharacterFloor},
+			{anim="dead" ,boundType= BoundType.CharacterFloor},
+		},		
+		peekBranchSet = 1,	
+	},	
 }
 
 return animdefs
