cefa5db2dea15fbbb622805f265e7ae12ba8ca82
Hekateras <39839566+Hekateras@users.noreply.github.com>
2021-05-18 22:04:50 +0200

    Fixed only mole being able to scrub the camera DB (copypaste holdover)

--- scripts/abilities/MM_scrubcameradb.lua
+++ scripts/abilities/MM_scrubcameradb.lua
@@ -82,9 +82,9 @@ local MM_scrubcameradb = --tweaked version of monster root hub hack
 				return false, STRINGS.UI.REASON.ALREADY_HACKING 
 			end 
 
-			if unit:getTraits().MM_mole ~= true then 
-				return false, STRINGS.ABILITIES.HACK_ONLY_MOLE --only mole
-			end 
+			-- if unit:getTraits().MM_mole ~= true then 
+				-- return false, STRINGS.ABILITIES.HACK_ONLY_MOLE --only mole
+			-- end 
 
 			if abilityOwner:getTraits().mainframe_ice > 0 then 
 				return false, STRINGS.ABILITIES.TOOLTIPS.UNLOCK_WITH_INCOGNITA
