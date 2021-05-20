a2de7a9ba4b7dd4759b39843437a98d559b05380
Hekateras <39839566+Hekateras@users.noreply.github.com>
2021-05-20 06:10:34 +0200

    fix

--- scripts/missions/distress_call.lua
+++ scripts/missions/distress_call.lua
@@ -360,7 +360,7 @@ local function startAgentEscape( script, sim, mission )
 		script:queue( { type="pan", x=x0, y=y0, zoom=0.27 } )
 		script:queue(2*cdefs.SECONDS) --without this Central's message gets "skipped" for some reason because of the agent stating oneliner still playing
 
-		if not sim:getParams().campaignDifficulty == simdefs.NORMAL_DIFFICULTY then
+		if not (sim:getParams().campaignDifficulty == simdefs.NORMAL_DIFFICULTY) then
 			makeGuardInvestigate(script, sim)
 		end
 		
