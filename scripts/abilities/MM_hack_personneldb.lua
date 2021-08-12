ca030fee064057479ec0f8d9ea22ed64ac3c3fed
Hekateras <39839566+Hekateras@users.noreply.github.com>
2021-08-12 23:44:20 +0200

    Add files via upload

--- scripts/abilities/MM_hack_personneldb.lua
+++ scripts/abilities/MM_hack_personneldb.lua
@@ -10,7 +10,7 @@ local MM_hack_personneldb = --tweaked version of monster root hub hack
 		name = STRINGS.MOREMISSIONS.ABILITIES.HACK_PERSONNELDB,
 
 		createToolTip = function( self, sim, unit )
-			return abilityutil.formatToolTip(STRINGS.MOREMISSIONS.ABILITIES.HACK_PERSONNELDB,STRINGS.MOREMISSIONS.ABILITIES.HACK_PERSONNELDB_DESC) 
+			return abilityutil.formatToolTip(STRINGS.MOREMISSIONS.ABILITIES.HACK_PERSONNELDB,STRINGS.MOREMISSIONS.ABILITIES.HACK_PERSONNELDB_DESC)
 		end,
 
 		proxy = true,
@@ -18,24 +18,28 @@ local MM_hack_personneldb = --tweaked version of monster root hub hack
 		getName = function( self, sim, abilityOwner, abilityUser, targetUnitID )
 			return self.name
 		end,
-		
+
 		profile_icon = "gui/icons/action_icons/Action_icon_Small/icon-item_hijack_small.png",
 
 		isTarget = function( self, abilityOwner, unit, targetUnit )
-		
+
 			if targetUnit:getTraits().mainframe_status ~= "active" then
 				return false
 			end
-			
+
+			-- if unit:getTraits().monster_hacking then
+				-- return false
+			-- end
+
 			return true
 		end,
 
 		acquireTargets = function( self, targets, game, sim, abilityOwner, unit )
 			local units = {}
-			
+
 			if sim:getTags().finished_DB_hack then
 				return targets.unitTarget( game, units, self, abilityOwner, unit )
-			end 
+			end
 
 			for _, targetUnit in pairs(sim:getAllUnits()) do
 				local x1, y1 = targetUnit:getLocation()
@@ -53,20 +57,20 @@ local MM_hack_personneldb = --tweaked version of monster root hub hack
 
 			if abilityOwner:getTraits().cooldown and abilityOwner:getTraits().cooldown > 0 then
 				return false,  util.sformat(STRINGS.UI.REASON.COOLDOWN,abilityOwner:getTraits().cooldown)
-			end	
+			end
 			if abilityOwner:getTraits().usesCharges and abilityOwner:getTraits().charges < 1 then
 				return false, util.sformat(STRINGS.UI.REASON.CHARGES)
-			end				
+			end
 
-			if unit:getTraits().monster_hacking == true then 
-				return false, STRINGS.UI.REASON.ALREADY_HACKING 
-			end 
+			-- if unit:getTraits().monster_hacking ~= nil then
+				-- return false, STRINGS.UI.REASON.ALREADY_HACKING
+			-- end
 
-			if unit:getTraits().MM_mole ~= true then 
+			if unit:getTraits().MM_mole ~= true then
 				return false, STRINGS.ABILITIES.HACK_ONLY_MOLE --only mole
-			end 
+			end
 
-			if abilityOwner:getTraits().mainframe_ice > 0 then 
+			if abilityOwner:getTraits().mainframe_ice > 0 then
 				return false, STRINGS.ABILITIES.TOOLTIPS.UNLOCK_WITH_INCOGNITA
 			end
 
@@ -77,18 +81,18 @@ local MM_hack_personneldb = --tweaked version of monster root hub hack
 			local targetUnit = sim:getUnit( targetUnitID )
 
 			local x0,y0 = unit:getLocation()
-			local x1,y1 = targetUnit:getLocation()			
+			local x1,y1 = targetUnit:getLocation()
 			local tempfacing = simquery.getDirectionFromDelta( x1 - x0, y1 - y0 )
-			unit:setFacing(tempfacing)	
+			unit:setFacing(tempfacing)
 			sim:dispatchEvent( simdefs.EV_UNIT_USECOMP, { unitID = unit:getID(), facing = tempfacing, sound = "SpySociety/Actions/monst3r_jackin" , soundFrame = 16, useTinkerMonst3r=true } )
 			sim:triggerEvent( "mole_DB_hack_start" )
 
 			unit:getTraits().monster_hacking = abilityOwner:getID() --trait to set looped tinker anim
-			abilityOwner:getTraits().progress = abilityOwner:getTraits().progress or 0 			
+			abilityOwner:getTraits().progress = abilityOwner:getTraits().progress or 0
 			unit:getSounds().spot = "SpySociety/Actions/monst3r_hacking"
 			sim:dispatchEvent( simdefs.EV_UNIT_REFRESH, { unit = unit })
 		end,
 
 	}
-	
+
 return MM_hack_personneldb
