0800ba277184375f6ac8cb003a339fcb9b466225
Hekateras <39839566+Hekateras@users.noreply.github.com>
2022-02-11 04:05:26 +0100

    fixed distress call alarm tracker bug

--- scripts/appended_functions/aiplayer.lua
+++ scripts/appended_functions/aiplayer.lua
@@ -7,3 +7,19 @@ local simquery = include ( "sim/simquery" )
 local abilityutil = include( "sim/abilities/abilityutil" )
 local cdefs = include( "client_defs" )
 local simdefs = include( "sim/simdefs" )
+
+local aiplayer = include( "sim/aiplayer" )
+local _onEndTurn = aiplayer.onEndTurn
+function aiplayer:onEndTurn(sim)
+	local trackerBoost = sim.missionTrackerBoost or 0
+	_onEndTurn(self, sim)
+	trackerBoost = 0
+end
+
+-- this bit was moved to engine_init append
+-- local simengine = include( "sim/engine" )
+-- local _trackerAdvance = simengine.trackerAdvance
+-- function simengine:trackerAdvance(delta, ...)
+	-- delta = delta + trackerBoost
+	-- return _trackerAdvance(self, delta, ...)
+-- end
