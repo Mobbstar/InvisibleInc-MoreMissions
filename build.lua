d0ad108876d21f45f6aadcb199c446c9b2f43c5a
Mobbstar <Mobbstar@users.noreply.github.com>
2022-03-06 18:35:22 +0100

    remove FDP from sound.kwad

--- build.lua
+++ build.lua
@@ -52,8 +52,7 @@ package:Save( build_options.outputpath, "gui.kwad" )
 -- AUDIO
 package = Package:new( "sounds", MaxPackageSize, options )
 PackageFolder( package, "./sound", true, function(srcpath)
-	return string.find(srcpath, ".fdp", -4) ~= nil
-		or string.find(srcpath, ".fev", -4) ~= nil
+	return string.find(srcpath, ".fev", -4) ~= nil
 		or string.find(srcpath, ".fsb", -4) ~= nil
 end )
 package:Save( build_options.outputpath, "sound.kwad" )
