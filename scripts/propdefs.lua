b320525924b9df0a54388b4d8a61383a25e2135f
Mobbstar <Mobbstar@users.noreply.github.com>
2023-09-04 20:34:13 +0200

    cleanup of scrapped ideas

--- scripts/propdefs.lua
+++ scripts/propdefs.lua
@@ -50,48 +50,7 @@ local prop_templates =
 {
 	-----------------------------------------------------
 	-- Level Props
-	
-	-- upgradestudio_speed = 
-	-- {
-		-- type = "simunit", 
-		-- name = STRINGS.ISTANBUL4.PROPS.UPGRADESTUDIO_SPEED,
-		-- onWorldTooltip = onDeviceTooltip,
-		-- rig ="corerig",
-		-- kanim = "kanim_switch", 
-		-- abilities = { "useUpgradeMachine" }, 
-		-- -- children = {  },
-		-- -- tags = {  }, --for Central flavourtext and instructions
-		-- traits = util.extend( MAINFRAME_TRAITS ){
-			-- moveToDevice = true,
-			-- cover = true,
-			-- impass = {0,0},
-			-- sightable = true,
-			-- skill = "speed",
-			-- -- isObjective = true,
-			-- -- multiLockSwitch = true,
-			-- -- noOpenAnim = true,
-			-- -- canKO = false,
-			-- -- mainframe_status = "inactive", -- So that it activates on spawn.
-			-- -- startOn = true,
-			-- -- mainframe_autodeactivate = true, 
-			-- -- spotSoundPowerDown = true,
-			-- -- agent_filter = true,
-			-- -- activate_txt_title=STRINGS.DLC1.OPEN_SECURITY_DOOR,
-			-- -- activate_txt_body=STRINGS.DLC1.OPEN_SECURITY_DOOR_DESC
-			-- -- cell_door = true
-			-- recap_icon = "access_switch",
-		-- },
-		-- sounds = {
-			-- appeared="SpySociety/HUD/gameplay/peek_positive", --peek_negative
-			-- spot="SpySociety_DLC001/Objects/router",
-			-- use="SpySociety/Actions/console_use",	
-			-- reboot_start="SpySociety/Actions/reboot_initiated_generator",
-			-- reboot_end="SpySociety/Actions/reboot_complete_generator",
-			-- switch_reset="SpySociety_DLC001/Actions/DLCswitch_reset",
-			-- computer_reset="SpySociety_DLC001/Actions/DLCswitch_computerreset",
-		-- }
-	-- },
-	
+
 	--custom one so no overlap
 	MM_hostage_capture_ea = 
 	{ 
@@ -215,7 +174,7 @@ local prop_templates =
 		sounds = {appeared="SpySociety/HUD/gameplay/peek_positive", reboot_start="SpySociety/Actions/reboot_initiated_generator",reboot_end="SpySociety/Actions/reboot_complete_generator", switch_reset="SpySociety_DLC001/Actions/DLCswitch_reset", computer_reset="SpySociety_DLC001/Actions/DLCswitch_computerreset" }
 	},	
 
-	
+
 	MM_vault_safe_1 = 
 	{ 
 		type = "simunit", 
